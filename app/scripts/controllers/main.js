9d3c422099c3dcfc27c58d3bbeb4f868d67c7518
Miguel Angulo <miguel.angulo@coderoad.com>
2015-03-15 10:18:34 -0400

    switch en el consumo de servicios

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,9 +16,9 @@ angular.module('hackatonApp')
     ];
     var thing = new ThingService();
     console.log("First");
-//    thing.loadDataAll({},function(data){
-//      console.log(JSON.stringify(data,null,4));
-//    })
+  thing.loadDataAll({},function(data){
+    console.log(JSON.stringify(data,null,4));
+   });
     /*$http.get('scripts/data.json').success(function (data) {
       //Convert data to array.
       //datos lo tenemos disponible en la vista gracias a $scope
@@ -35,13 +35,7 @@ angular.module('hackatonApp')
             });
         }, 800);
 
-
-
-
         //location.reload();
-
-
-
     //datosResource lo tenemos disponible en la vista gracias a $scope
 //    $scope.datosResource = dataResource.get();
 //    console.log(JSON.stringify($scope.datos,null,4));
